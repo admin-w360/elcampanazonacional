a99fbc6fcbaa08c1b5022648bcb96469bb9fcc4d
admin <admin@w360.co>
2022-11-30 10:44:33 -0500

    update contact form

--- resources/js/pages/sites/contacts/services/index.ts
+++ resources/js/pages/sites/contacts/services/index.ts
@@ -6,5 +6,5 @@ import http from "@/utils/request";
  * 登录
  */
 export function postContact(params: any = []): Promise<any> {
-    return http.post('download', params)
+    return http.post('contacts', params)
 }
